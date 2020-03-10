62263e438e12daed5e32e1269e89b93aaf3cff0a
Malkowski, Maciej (Agoda) <Maciej.Malkowski@agoda.com>
2020-03-10 18:10:46 +0700

    feat!: change to cli with params based approach vs gh api

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
+const program = require('commander');
+
 const path = require("path");
 const selfPkg = require(path.resolve(__dirname, './package.json'));
 
 const detectService = require('./lib/detectService')
-const getGithubInfo = require('./lib/getGithubInfo')
-const { getMasterConfig, getPullConfig } = require('./lib/getSuffix')
 const { addSuffix } = require('./lib/packageJsonManager')
 
 const run = async () => {
+    program
+        .version(selfPkg.version)
+        .option('--canary <suffix>', 'Tags packages as canary release with provided suffix')
+        .option('--buildNumber <number>', 'Adds build number to suffix when canary release')
+
+    program.parse(process.argv);
+
     console.log(`Welcome to package-version-suffix ver. ${selfPkg.version}`)
-    const { buildCounter, branch, slug, service, setEnvVariable, setBuildNumber } = detectService();
-    const [owner, repo] = slug.split('/')
 
-    console.log(`Detected ${service} service with following config:`)
-    console.log(`Working branch: ${branch}, build.counter: ${buildCounter}, github repo: ${slug}`)
-    const [branchTCName, branchTCNumber] = branch.split('/');
+    const { service, setBuildNumber } = detectService();
+
+    console.log(`Detected ${service} service`)
     
-    const suffixAndTag = await retrieveSuffixConvention(branchTCName, branchTCNumber, buildCounter, owner, repo)
-    if (suffixAndTag.suffix !== '') {
-        setBuildNumber(suffixAndTag.suffix.substr(1))
-        await addSuffix(suffixAndTag.suffix);
+    if (program.canary) {
+        if (!program.buildNumber) {
+            console.error('Failed to parse build number when canary param was present')
+            process.exit(1)
+        } else {
+            console.log(`Selected canary build. Adding defined suffixes to packages.`)
+            setBuildNumber(`${program.canary}.${program.buildNumber}`)
+            await addSuffix(`-${program.canary}.${program.buildNumber}`);
+        }
     } else {
-        setBuildNumber(`release.${buildCounter}`)
-    }
-    setEnvVariable('npmTag', suffixAndTag.tag)
-}
-
-const retrieveSuffixConvention = async (branchTCName, branchTCNumber, buildNumber, owner, repo) => {
-    if (branchTCName === 'master') {
-        console.log(`Looks like build is running on master branch, setting tag to latest`)
-        return getMasterConfig();
-    } else if (branchTCName === 'pull'){
-        console.log(`Looks like build is running on pull request branch, going to fetch info from github about the pull request (#${branchTCNumber})`)
-        const { pullBranch } = await getGithubInfo(owner, repo, branchTCNumber);
-        return getPullConfig(pullBranch, branchTCNumber, buildNumber)
-    } else {
-        console.log(`Looks like its running on some custom branch, failing!`);
-        process.exit(1)
+        console.log(`No params passed. Skipping suffix modification, tagging build`)
+        setBuildNumber(`release.${program.buildNumber}`)
     }
 }
 
